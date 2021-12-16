3d1558292a1285685423ef8712d0a8e1329965f0
Vivek <vvivksingh@gmail.com>
2021-12-16 08:38:56 +0530

    commited locally

--- Demo.java
+++ Demo.java
@@ -4,5 +4,6 @@ class Demo{
 		System.out.println("Hello World !");
 		// this file is edited in IntelliJ and pushed using IntellijJ teriminal
 		// This line is added on remote repository.
+		//this line is added locally to raise conflictgit
 	}
 }
